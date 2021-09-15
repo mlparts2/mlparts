264035fd878f458bbbc920afe15bb68ffcd2542e
mlparts2 <90757823+mlparts2@users.noreply.github.com>
2021-09-15 12:08:32 +0200

    Delete script.js.js

--- kontaktni-formular/script.js.js
+++
@@ -1,4 +0,0 @@
-
-$(".layout-col-content").html('<div class="c-contact-form"><div class="container"><div class="c-contact-inner"><div class="c-contact-left"><div class="c-contact-left-inner"><h2>Kontaktujte nás</h2><div class="c-contact-qcontact"> <img src="https://mlparts.github.io/rozvoz/icon-phone.svg"><div class="c-contact-qcontact-content"> <strong>+420 722 537 981</strong> <span>pondělí–pátek 8:00 – 17:30</span></div></div><div class="c-contact-qcontact"> <img src="https://mlparts.github.io/rozvoz/icon-envelope.svg"><div class="c-contact-qcontact-content"> <strong>info@mlparts.cz</strong></div></div></div></div><div class="c-contact-right"><div id="cContactForm"></div></div></div></div></div>');
-
-$("#cContactForm").load("/kontakt/ #frm-contactForm");
