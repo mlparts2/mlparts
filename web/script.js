5eac31a0cd7bdece8f9cec266f55c149dff8fa79
mlparts2 <90757823+mlparts2@users.noreply.github.com>
2022-01-13 15:46:19 +0100

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+    $("body").prepend('<a class="chatbox" href="/kontakt#frm-contactForm"><div class="chatbox-top"><img src="https://mlparts.github.io/web/arrow.svg?v=1"><span>Potřebujete poradit?</span></div><div class="chatbox-inner"><img src="https://mlparts.github.io/web/chat2.svg"><span>Napište nám</span></div></a>');
+});
